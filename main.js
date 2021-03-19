6cc731829a53185c6663c795bf7b7c7474a0b542
unknown <evgenshtanko@ya.ru>
2021-03-19 11:11:30 +0400

    t

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 function range() {
-    let rangInput = document.querySelectorAll(".range__input");
+    const rangInput = document.querySelectorAll(".range__input");
+
+    function startPositionRange() {
+        for (let i = 0; i < rangInput.length; i++) {
+            const element = rangInput[i];
+            const val = element.value;
+            const min = element.getAttribute('min');
+            const max = element.getAttribute('max');
+            const step = element.getAttribute('step');
+            const position = 100 / (max - step) * (val - step);
+            
+            element.parentNode.querySelector('.range__track').style.width = position + '%';
+        }
+    }
+
+    startPositionRange();
 
     for (let i = 0; i < rangInput.length; i++) {
-        const element = rangInput[i];
+        let element = rangInput[i];
         element.oninput = function () {
-            let val = this.value;
-            let min = this.getAttribute('min');
-            let max = this.getAttribute('max');
-            let step = this.getAttribute('step');
-            let position = 100 / (max - step) * (val - step);
+            const val = this.value;
+            const min = this.getAttribute('min');
+            const max = this.getAttribute('max');
+            const step = this.getAttribute('step');
+            const position = 100 / (max - step) * (val - step);
             
             this.parentNode.querySelector('.range__track').style.width = position + '%';
         }
